9facc51f2ab33d5f52a5aa904fc11a11ff743aaf
Shubhaankar Sharma <66492212+Shubhaankar-Sharma@users.noreply.github.com>
2022-06-29 09:07:07 +0530

    initial draft for options

--- httpmock.go
+++ httpmock.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+type Option string
+
+const OverrideHeaderMatchOption Option = "override_header_match"
+
+func (opt *Option) String() string {
+	return string(*opt)
+}
+
 type MockResponse struct {
 	Request  http.Request
 	Response Response
@@ -13,6 +21,7 @@ type MockResponse struct {
 type MockHTTPServer struct {
 	Listener    net.Listener
 	ResponseMap map[string]Response
+	Opts        []Option
 }
 
 type Response struct {
@@ -25,11 +34,21 @@ func NewMockHTTPServer(b ...string) *MockHTTPServer {
 	var err error
 	m := &MockHTTPServer{
 		ResponseMap: make(map[string]Response),
+		Opts:        make([]Option, 0),
 	}
+
 	if len(b) == 0 {
 		m.Listener, err = net.Listen("tcp", ":9001")
 	} else {
 		m.Listener, err = net.Listen("tcp", b[0])
+	}
+	if len(b) > 1 {
+		for _, option := range b[1:] {
+			switch Option(option) {
+			case OverrideHeaderMatchOption:
+				m.Opts = append(m.Opts, OverrideHeaderMatchOption)
+			}
+		}
 	}
 
 	if err != nil {
